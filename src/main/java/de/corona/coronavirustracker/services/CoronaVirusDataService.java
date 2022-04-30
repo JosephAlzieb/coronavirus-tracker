0e6b99cac011f0e11a27980d94902c1e960a351c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-30 14:43:10 +0200

    this method should connect the CSSEIGSandData-Repository to get the data as csv-file. this file will be every day updated, so we get just the update and the previous day.

---
+++ src/main/java/de/corona/coronavirustracker/services/CoronaVirusDataService.java
@@ -0,0 +1,46 @@
+package de.corona.coronavirustracker.services;
+
+import de.corona.coronavirustracker.models.LocationStats;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoronaVirusDataService {
+
+  private static final String CORON_VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+  public ArrayList fetchVirusData() throws IOException, InterruptedException {
+    ArrayList allStats = new ArrayList();
+    HttpClient client = HttpClient.newHttpClient();
+
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(CORON_VIRUS_DATA_URL))
+        .build();
+
+    HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+    StringReader csvBodyReader = new StringReader(httpResponse.body());
+
+    Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+    for (CSVRecord record : records) {
+      LocationStats locationStat = new LocationStats();
+      locationStat.setState(record.get("Province/State"));
+      locationStat.setCountry(record.get("Country/Region"));
+      int latestCases = Integer.parseInt(record.get(record.size() - 1));
+      int prevDayCases = Integer.parseInt(record.get(record.size() - 2));
+      locationStat.setLatestTotalCases(latestCases);
+      locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+
+      allStats.add(locationStat);
+    }
+
+    return allStats;
+  }
+}
