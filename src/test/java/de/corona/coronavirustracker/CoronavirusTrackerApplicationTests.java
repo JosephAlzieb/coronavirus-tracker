ba8f8ca79f3354dd08c29f149f3c7a9b99bac96a
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-30 14:00:06 +0200

    initial project

---
+++ src/test/java/de/corona/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.corona.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
